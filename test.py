6a36a6c07c66facf69796a676408d0df01771c3b
MengshiLi <mengshili96@gmail.com>
2022-03-21 12:41:42 -0400

    try

---
+++ test.py
@@ -0,0 +1,19 @@
+import socket
+
+
+
+
+# host = "david.choffnes.com"
+# path = "/classes/cs4700sp22/project4.php"
+#
+# socket = socket.socket()
+# socket.connect((host,80))
+#
+# get_msg = 'GET ' + path + ' HTTP/1.1\r\n' + 'Host: ' + host + '\r\n\r\n'
+#
+# socket.send(get_msg.encode())
+#
+# print(socket.recv(1024).decode())
+
+# a = (2 << 4)
+# print(a)
