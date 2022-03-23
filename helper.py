2e0eaeb8cd6f84c672da567d8ddc2a01a1c11e0c
MengshiLi <mengshili96@gmail.com>
2022-03-22 20:06:46 -0400

    debug

--- helper.py
+++ helper.py
@@ -57,6 +57,23 @@ def split_data_to_send(data, segment_size, tcp_seq):
 
     return result
 
+def parse_url(url):
+    url = url.replace("http://", "")
+    url = url.replace("https://", "")
+    url_list = url.split("/")
+    host = url_list[0]
+
+    if len(url_list) == 1 or url_list[1] == "":
+        file = "index.html"
+    else:
+        file = url_list[len(url_list) - 1]
+
+    path = url.replace(host, "")
+    if path == "":
+        path = "/"
+
+    return host, file, path
+
 
 # print(split_data_to_send("1234567891234567891234567891234",9,0))
 print(get_tcp_flags(ack=1))
