34ea81307643cecb9c0180d42c0262e37daf5383
MengshiLi <mengshili96@gmail.com>
2022-03-24 15:39:18 -0400

    debug

--- new_socket.py
+++ new_socket.py
@@ -182,9 +182,9 @@ class RawSocket:
             tcp_data = self._recv()
             tcp_data.print()
             print(tcp_data.payload)
-            self.tcp_seq = tcp_data.tcp_ack_seq
-            self.tcp_ack = tcp_data.tcp_seq + 1
-            self._send('', get_tcp_flags(ack=1))
+            # self.tcp_seq = tcp_data.tcp_ack_seq
+            # self.tcp_ack = tcp_data.tcp_seq + 1
+            # self._send('', get_tcp_flags(ack=1))
             # print("this is ACK after we receive a bag")
 
         # print(tcp_data.payload)
