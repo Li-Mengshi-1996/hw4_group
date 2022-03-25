97c6dccb9f9f9dbd43bb1be0434828cf77e79d7b
MengshiLi <mengshili96@gmail.com>
2022-03-25 14:44:48 -0400

    debug

--- check.py
+++ check.py
@@ -5,6 +5,6 @@ lines1 = file1.readlines()
 lines2 = file2.readlines()
 
 for i in range(len(lines1)):
-    if  lines1[i] != lines2[i]:
+    if i >= len(lines2) or lines1[i] != lines2[i]:
         print(i)
         print(lines1[i])
