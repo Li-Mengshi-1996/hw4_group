0b1df4bdef2787f744fbd71c69852867bc04838d
MengshiLi <mengshili96@gmail.com>
2022-03-25 16:22:10 -0400

    debug

--- check_log.py
+++ check_log.py
@@ -3,15 +3,15 @@ file2 = open("2MB_check.log","rb")
 
 print(file1)
 
-lines1 = file1.read()
-lines2 = file2.read()
+lines1 = file1.readlines()
+lines2 = file2.readlines()
+#
+# print(lines1)
 
-print(lines1)
+for i in range(len(lines1)):
+    if i >= len(lines2) or lines1[i] != lines2[i]:
+        print(i)
+        print(lines1[i])
 
-# for i in range(len(lines1)):
-#     if i == 0:
-#         print("i")
-#         print(lines1[i])
-#     if i >= len(lines2) or lines1[i] != lines2[i]:
-#         print(i)
-#         print(lines1[i])
+print(lines2[len(lines1)])
+# \xc6\xdb\xa3\x93)\x88\x80\x1c\xe5\xba"\x87\xb2\x98\xe5\xd5W\x86es\x83\x01!;G\xf8\xc2:\t\x13\x94L\x81\x84bA~\x955\x19\x16BcGN
