ec0bb8d1f9d6c6645ae50d28f5ebba31e34905b9
MengshiLi <mengshili96@gmail.com>
2022-03-25 14:46:56 -0400

    debug

--- check.py
+++ check.py
@@ -4,7 +4,11 @@ file2 = open("test.php")
 lines1 = file1.readlines()
 lines2 = file2.readlines()
 
+count = 500
+
 for i in range(len(lines1)):
+    if i>= count:
+        break
     if i >= len(lines2) or lines1[i] != lines2[i]:
         print(i)
         print(lines1[i])
