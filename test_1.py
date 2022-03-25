7a2f3abb23a7bccfbe3756698640f30cb9266266
MengshiLi <mengshili96@gmail.com>
2022-03-25 16:02:42 -0400

    debug

--- test_1.py
+++ test_1.py
@@ -1,1 +1,17 @@
-print(5 & 1)
+import socket
+from helper import *
+
+host, file_name, path = parse_url("https://david.choffnes.com/classes/cs4700sp22/project4.php")
+request = 'GET ' + path + ' HTTP/1.1\r\n' + 'Host: ' + host + '\r\n\r\n'
+s = socket.socket()
+s.connect((host, 80))
+s.send(request.encode())
+content = ""
+while True:
+    content += s.recv(65536).decode()
+
+    if content.find("/html>") != -1:
+        break
+
+with open("test.php", 'w') as file:
+    file.write(content)
