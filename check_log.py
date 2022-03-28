38064ccc5d6584ffead6699f765ab651fc5aea7b
MengshiLi <mengshili96@gmail.com>
2022-03-28 13:59:12 -0400

    debug

--- check_log.py
+++ check_log.py
@@ -1,5 +1,5 @@
-file1 = open("50MB.log", "rb")
-file2 = open("50MB_check.log", "rb")
+file1 = open("2MB_test.log", "rb")
+file2 = open("2MB_check.log", "rb")
 
 print(file1)
 
@@ -18,5 +18,5 @@ for i in range(len(lines1)):
 
 # print(lines2[len(lines1)])
 
-print(len(lines1) - len(lines2))
+print(len(lines1))
 # \xc6\xdb\xa3\x93)\x88\x80\x1c\xe5\xba"\x87\xb2\x98\xe5\xd5W\x86es\x83\x01!;G\xf8\xc2:\t\x13\x94L\x81\x84bA~\x955\x19\x16BcGN
