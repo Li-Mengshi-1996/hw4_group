5a28b6b07222d6c56dde02f4e645a1dfecf37025
MengshiLi <mengshili96@gmail.com>
2022-03-28 14:08:48 -0400

    debug

---
+++ remove_header.py
@@ -0,0 +1,13 @@
+file1 = open("2MB_try.log", "rb")
+# file2 = open("2MB_check.log", "rb")
+
+
+lines1 = file1.readlines()[11:]
+
+content = b""
+
+for line in lines1:
+    content += line
+
+with open("2MB_clean.log", 'wb') as file:
+    file.write(content)
