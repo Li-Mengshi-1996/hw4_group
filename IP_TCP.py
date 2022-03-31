2e083a28ca187a6c87725861f63066bf48b3d8dc
MengshiLi <mengshili96@gmail.com>
2022-03-31 12:19:03 -0400

    debug

--- IP_TCP.py
+++ IP_TCP.py
@@ -78,6 +78,8 @@ def extract_tcp_header(data):
     adwnd = socket.ntohs(tcp_window)
     payload = data[20:]
 
+    print("check when create: " + str(tcp_check))
+
     result = TCPHeader(tcp_source, tcp_dest, tcp_seq, tcp_ack_seq, tcp_flags, tcp_doff, adwnd, tcp_urg_ptr, payload, tcp_check)
 
     return result
