c5583d1c21a7c6bd7ee2949124a93c8744b26991
MengshiLi <mengshili96@gmail.com>
2022-04-01 14:47:14 -0400

    debug

--- new_socket.py
+++ new_socket.py
@@ -170,13 +170,12 @@ class RawSocket:
             return None
         # psh = get_pseudo_ip_header(self.destination_ip, self.source_ip, len(tcp_data))
 
-        if choice ==1:
-            if not check_tcp(tcp_data, self.source_ip, self.destination_ip):
-                # self._send("", get_tcp_flags(ack=1))
-                result = extract_tcp_header(tcp_data)
-                result.tcp_flags = -1
-                return result
-
+        # if choice == 1:
+        #     if not check_tcp(tcp_data, self.source_ip, self.destination_ip):
+        #         # self._send("", get_tcp_flags(ack=1))
+        #         result = extract_tcp_header(tcp_data)
+        #         result.tcp_flags = -1
+        #         return result
 
         # print("check: " + str(calculate_checksum(psh + tcp_data)))
 
@@ -190,7 +189,7 @@ class RawSocket:
         print("receive")
 
         while True:
-            tcp_data = self._recv(delay=180,choice=1)
+            tcp_data = self._recv(delay=180, choice=1)
             if tcp_data is None:
                 print("Connection lost.")
                 sys.exit(1)
