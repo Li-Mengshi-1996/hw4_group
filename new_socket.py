e1d71ae6472195cd8fb8e6295088645a66cda296
MengshiLi <mengshili96@gmail.com>
2022-04-01 14:51:21 -0400

    debug

--- new_socket.py
+++ new_socket.py
@@ -297,63 +297,63 @@ class RawSocket:
 #
 #
 #
-def main():
-    # host, file_name, path = parse_url("https://david.choffnes.com/classes/cs4700sp22/project4.php")
-    # host, file_name, path = parse_url("https://david.choffnes.com/classes/cs4700sp22/2MB.log")
-    # host, file_name, path = parse_url("https://david.choffnes.com/classes/cs4700sp22/10MB.log")
-    host, file_name, path = parse_url("https://david.choffnes.com/classes/cs4700sp22/50MB.log")
-    # host, file_name, path = parse_url("https://david.choffnes.com/classes/cs4700sp22/")
-    # print(file_name)
-    # return
-    t = RawSocket()
-    t.connect(host)
-
-    #
-    request = 'GET ' + path + ' HTTP/1.1\r\n' + 'Host: ' + host + '\r\n\r\n'
-
-    t.send(request)
-    content = t.receive()
-
-    print(content[0:200])
-
-    if not content.startswith(b"HTTP/1.1 200 OK"):
-        print("Non-200 status code")
-        sys.exit()
-
-    left = content.find(b'4000\r\n')
-    right = left + len(b'4000\r\n')
-
-    if left != -1:
-        content = content[0:left] + content[right:]
-
-    content = re.sub(rb'\r\n0\r\n\r\n', b"", content)
-
-    # content = re.sub(rb'\r\n[0-9]\d*\r\n', b"", content)
-    #
-    # content = re.sub(rb'\r\n4f\w\r\n', b"", content)
-
-    temp = content.split(b"\r\n")
-    diff = len(b"\r\n")
-
-    result = b""
-
-    for item in temp:
-        try:
-            in_ten = int(item, 16)
-            result = result[0:len(result) - diff]
-            print("find garbage")
-        except:
-            result = result + item + b"\r\n"
-
-    content = result[0:len(result) - diff]
-
-    parse = content.find(b"\r\n\r\n")
-    content = content[parse + len(b"\r\n\r\n"):]
-
-    t.close()
-
-    with open(file_name, 'wb') as file:
-        file.write(content)
-
-
-main()
+# def main():
+#     # host, file_name, path = parse_url("https://david.choffnes.com/classes/cs4700sp22/project4.php")
+#     # host, file_name, path = parse_url("https://david.choffnes.com/classes/cs4700sp22/2MB.log")
+#     # host, file_name, path = parse_url("https://david.choffnes.com/classes/cs4700sp22/10MB.log")
+#     host, file_name, path = parse_url("https://david.choffnes.com/classes/cs4700sp22/50MB.log")
+#     # host, file_name, path = parse_url("https://david.choffnes.com/classes/cs4700sp22/")
+#     # print(file_name)
+#     # return
+#     t = RawSocket()
+#     t.connect(host)
+#
+#     #
+#     request = 'GET ' + path + ' HTTP/1.1\r\n' + 'Host: ' + host + '\r\n\r\n'
+#
+#     t.send(request)
+#     content = t.receive()
+#
+#     print(content[0:200])
+#
+#     if not content.startswith(b"HTTP/1.1 200 OK"):
+#         print("Non-200 status code")
+#         sys.exit()
+#
+#     left = content.find(b'4000\r\n')
+#     right = left + len(b'4000\r\n')
+#
+#     if left != -1:
+#         content = content[0:left] + content[right:]
+#
+#     content = re.sub(rb'\r\n0\r\n\r\n', b"", content)
+#
+#     # content = re.sub(rb'\r\n[0-9]\d*\r\n', b"", content)
+#     #
+#     # content = re.sub(rb'\r\n4f\w\r\n', b"", content)
+#
+#     temp = content.split(b"\r\n")
+#     diff = len(b"\r\n")
+#
+#     result = b""
+#
+#     for item in temp:
+#         try:
+#             in_ten = int(item, 16)
+#             result = result[0:len(result) - diff]
+#             print("find garbage")
+#         except:
+#             result = result + item + b"\r\n"
+#
+#     content = result[0:len(result) - diff]
+#
+#     parse = content.find(b"\r\n\r\n")
+#     content = content[parse + len(b"\r\n\r\n"):]
+#
+#     t.close()
+#
+#     with open(file_name, 'wb') as file:
+#         file.write(content)
+#
+#
+# main()
