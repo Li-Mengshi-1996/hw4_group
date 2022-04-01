d731902680244f41957801e8ba9babf6f2887274
MengshiLi <mengshili96@gmail.com>
2022-04-01 16:24:25 -0400

    debug

--- IP_TCP.py
+++ IP_TCP.py
@@ -3,24 +3,6 @@ from struct import *
 from helper import *
 
 
-class IP_Info():
-    def __init__(self, ip_unpack):
-        self.source = socket.inet_ntoa(ip_unpack[8])
-        self.destination = socket.inet_ntoa(ip_unpack[9])
-
-
-class TCP_Info():
-    def __init__(self, tcp_unpack):
-        self.source = tcp_unpack[0]
-        self.destination = tcp_unpack[1]
-
-        self.seq = tcp_unpack[2]
-        self.ack_seq = tcp_unpack[3]
-        self.advertised_window = tcp_unpack[6]
-
-        self.flag = tcp_unpack[5]
-
-
 def extract_ip_header(data):
     ip_ihl_ver, ip_tos, ip_tot_len, ip_id, ip_frag_off, ip_ttl, ip_proto, ip_check, ip_saddr, ip_daddr = unpack(
         '!BBHHHBBH4s4s', data[0:20])
