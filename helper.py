a21dd86b342875fb216bdf5ea9d708d2e2b36c5a
MengshiLi <mengshili96@gmail.com>
2022-04-03 13:31:12 -0400

    comment

--- helper.py
+++ helper.py
@@ -1,23 +1,43 @@
 import socket
-import random
 from struct import *
 import array
 
 
 def get_tcp_flags(fin=0, syn=0, rst=0, psh=0, ack=0, urg=0):
+    """
+    Generate tcp flags.
+    :param fin: whether there is a FIN flag, 1 for yes, 0 for no
+    :param syn: whether there is an SYN flag, 1 for yes, 0 for no
+    :param rst: whether there is a RST flag, 1 for yes, 0 for no
+    :param psh: whether there is a PSH flag, 1 for yes, 0 for no
+    :param ack: whether there is an ACK flag, 1 for yes, 0 for no
+    :param urg: whether there is a URG flag, 1 for yes, 0 for no
+    :return: tcp flag
+    """
     return fin + (syn << 1) + (rst << 2) + (psh << 3) + (ack << 4) + (urg << 5)
 
 
 def get_source_address():
+    """
+    Get local ip address and an available port.
+    :return: a tuple (ip_address, port)
+    """
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(('8.8.8.8', 80))
     address = s.getsockname()
     s.close()
-    # print(address)
     return address
 
 
 def create_psh(source_ip, destination_ip, protocol, tcp_length):
+    """
+    Create a pseudo header.
+    :param source_ip: the source ip of the header
+    :param destination_ip: the destination ip of the header
+    :param protocol: IP protocol this header use.
+    :param tcp_length: the length of tcp header and the data.
+    :return: a pseudo header
+    """
     source_address = socket.inet_aton(source_ip)
     dest_address = socket.inet_aton(destination_ip)
     placeholder = 0
@@ -27,6 +47,11 @@ def create_psh(source_ip, destination_ip, protocol, tcp_length):
 
 
 def check_sum(msg):
+    """
+    Calculate the checksum of msg.
+    :param msg: data to be calculated checksum
+    :return: the checksum of msg
+    """
     s = 0
     if len(msg) % 2 != 0:
         msg += b'\0'
@@ -41,32 +66,16 @@ def check_sum(msg):
     return s
 
 
-# def split_data_to_send(data, segment_size, tcp_seq):
-#     start = 0
-#     temp = []
-#     result = []
-#     current_seq = tcp_seq
-#
-#     while start < len(data):
-#         end = min(start + segment_size, len(data))
-#         temp.append(data[start:end])
-#         start = end
-#
-#     for data_piece in temp:
-#         result.append((current_seq, data_piece))
-#         current_seq += len(data_piece)
-#
-#     print(result)
-#     return result
-
-
 def parse_url(url):
+    """
+    Parse the url and get the host and path of the url and get the file name to save data.
+    :param url: the url to be parsed
+    :return: the host and path of the url and get the file name to save data
+    """
     url = url.replace("http://", "")
     url = url.replace("https://", "")
     url_list = url.split("/")
     host = url_list[0]
-
-    print(url_list)
 
     if url.endswith("/") or len(url_list) == 1 or url_list[1] == "":
         file = "index.html"
@@ -80,84 +89,50 @@ def parse_url(url):
     return host, file, path
 
 
-def tcp_checksum(msg):
-    if len(msg) & 1:
-        msg = msg + b'\0'
-    words = array.array('h', msg)
-
-    s = 0
-    for word in words:
-        s = s + (word & 0xffff)
-
-    s = (s >> 16) + (s & 0xffff)
-    s = s + (s >> 16)
-    return (~s) & 0xffff
-
-
 def check_tcp(data, source_ip, destination_ip):
-    [tcp_source_port, tcp_destination_port, tcp_seq, tcp_ack_seq, tcp_offset_res, tcp_flags, tcp_window] = unpack('!HHLLBBH', data[0:16])
-    [tcp_check] = unpack('H', data[16:18])
-    [tcp_urg_ptr] = unpack('!H', data[18:20])
+    """
+    Calculate the tcp checksum when we get data.
+    :param data: data we get
+    :param source_ip: the source ip address
+    :param destination_ip: the destination ip
+    :return: whether the checksum we calculate is the same as the checksum the data has
+    """
 
+    # Parse the TCP header
+    [tcp_source_port, tcp_destination_port, tcp_seq, tcp_ack_seq, tcp_offset_res, tcp_flags, tcp_window] = unpack(
+        '!HHLLBBH', data[0:16])
+    [tcp_check] = unpack('H', data[16:18])
+
+    # Based on the info we get from TCP header, generate info we need.
     tcp_doff = tcp_offset_res >> 4
     payload = data[tcp_doff * 4:]
     saddr = socket.inet_aton(source_ip)
     daddr = socket.inet_aton(destination_ip)
     placeholder = 0
     protocol = socket.IPPROTO_TCP
-    # tcp_length should be the length inside the header * 4 and plust the length of the data
     tcp_length = tcp_doff * 4 + len(payload)
 
+    # Create pseudo header.
     psh = pack('!4s4sBBH', saddr, daddr, placeholder, protocol, tcp_length)
     tcp_data = data[:16] + pack('H', 0) + data[18:]
-    temp = psh + tcp_data
-    new_checksum = tcp_checksum(temp)
+    msg = psh + tcp_data
+
+    # Calculate the checksum.
+    if len(msg) & 1:
+        msg = msg + b'\0'
+    infos = array.array('h', msg)
+
+    s = 0
+    for info in infos:
+        s = s + (info & 0xffff)
+
+    s = (s >> 16) + (s & 0xffff)
+    s = s + (s >> 16)
+
+    cal_checksum = (~s) & 0xffff
 
     print("check")
     print(tcp_check)
-    print(new_checksum)
+    print(cal_checksum)
 
-    return tcp_check == new_checksum
-
-
-
-
-
-
-
-
-
-
-    # tcp_source_port, tcp_dest_port, tcp_seq, tcp_ack_seq, tcp_offset_res, tcp_flags, tcp_window, tcp_check, tcp_urg_ptr \
-    #     = unpack('!HHLLBBHHH', data[:20])
-    # tcp_doff = tcp_offset_res >> 4
-    #
-    # payload = data[20:]
-    #
-    # saddr = socket.inet_aton(source_ip)
-    # daddr = socket.inet_aton(destination_ip)
-    #
-    # # total_length = tcp_doff * 4 + len(payload)
-    # #
-    # # psh = pack('!4s4sBBH', saddr, daddr, 0, socket.IPPROTO_TCP, total_length)
-    # #
-    # # psh = psh + data[:16] + pack('H', 0) + data[18:]
-    #
-    # tcp_header = pack('!HHLLBBHHH', tcp_source_port, tcp_dest_port, tcp_seq, tcp_ack_seq, tcp_offset_res,
-    #                   tcp_flags, tcp_window, 0, tcp_urg_ptr)
-    #
-    # tcp_length = len(tcp_header) + len(payload)
-    #
-    # psh = create_psh(source_ip, destination_ip, socket.IPPROTO_TCP, tcp_length)
-    # psh = psh + tcp_header + payload
-    #
-    # we_check = check_sum(psh)
-    #
-    #
-    #
-    # print("check:")
-    #
-    # print(tcp_check & we_check)
-
-# print(split_data_to_send("1234567891234567891234567891234",9,0))
-# print(get_tcp_flags(ack=1))
+    return tcp_check == cal_checksum
--- test_1.py
+++ test_1.py
@@ -1,17 +1,49 @@
 import socket
-from helper import *
+# from helper import *
+#
+# host, file_name, path = parse_url("https://david.choffnes.com/classes/cs4700sp22/project4.php")
+# request = 'GET ' + path + ' HTTP/1.1\r\n' + 'Host: ' + host + '\r\n\r\n'
+# s = socket.socket()
+# s.connect((host, 80))
+# s.send(request.encode())
+# content = ""
+# while True:
+#     content += s.recv(65536).decode()
+#
+#     if content.find("/html>") != -1:
+#         break
+#
+# with open("test.php", 'w') as file:
+#     file.write(content)
 
-host, file_name, path = parse_url("https://david.choffnes.com/classes/cs4700sp22/project4.php")
-request = 'GET ' + path + ' HTTP/1.1\r\n' + 'Host: ' + host + '\r\n\r\n'
-s = socket.socket()
-s.connect((host, 80))
-s.send(request.encode())
-content = ""
-while True:
-    content += s.recv(65536).decode()
 
-    if content.find("/html>") != -1:
-        break
+# def split_data_to_send(data, segment_size, tcp_seq):
+#     start = 0
+#     temp = []
+#     result = []
+#     current_seq = tcp_seq
+#
+#     while start < len(data):
+#         end = min(start + segment_size, len(data))
+#         temp.append(data[start:end])
+#         start = end
+#
+#     for data_piece in temp:
+#         result.append((current_seq, data_piece))
+#         current_seq += len(data_piece)
+#
+#     print(result)
+#     return result
 
-with open("test.php", 'w') as file:
-    file.write(content)
+# def tcp_checksum(msg):
+#     if len(msg) & 1:
+#         msg = msg + b'\0'
+#     infos = array.array('h', msg)
+#
+#     s = 0
+#     for info in infos:
+#         s = s + (info & 0xffff)
+#
+#     s = (s >> 16) + (s & 0xffff)
+#     s = s + (s >> 16)
+#     return (~s) & 0xffff
